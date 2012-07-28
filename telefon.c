6738aefb8cda98f646e4cafb749d1af73d0c035f
jaseg <s@jaseg.de>
2012-07-28 15:27:26 +0200

    Added GPLv3-headers to telfon.c and foo.py

--- telefon.c
+++ telefon.c
@@ -1,9 +1,28 @@
+/*
+    c_locc: The all new LDAP-connected cybernetic lock
+    Copyright 2012 jaseg <s@jaseg.de> & lynxis <lynxis@c-base.org>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/pgmspace.h>
 #include <avr/interrupt.h>
 #include "uart.h"
 #include "locc.h"
+#include "keypad.c"
 
 void setup(void);
 void loop(void);
